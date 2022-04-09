f1eab2e613118a42960bdc2cecc103edb35cade5
Ricardo Olarte <olarte.jose.r@gmail.com>
2022-04-09 09:57:48 -0500

    Scripts

--- Scripts/Creación Tabla.sql
+++ Scripts/Creación Tabla.sql
@@ -1,39 +1,63 @@
+--TABLES
 create table usuarios(
 
-documento INT,
-nombre VARCHAR(20),
-contraseña VARCHAR(16),
-correo VARCHAR(20),
+documento INT NOT NULL,
+nombre VARCHAR(20) NOT NULL,
+contraseña VARCHAR(16) NOT NULL,
+correo VARCHAR(20) NOT NULL,
 telefono VARCHAR(20),
-tipousuario VARCHAR(3)
+tipousuario VARCHAR(3) NOT NULL
 
 );
 
 create table tipo_usuario(
 
-id_tipousuario VARCHAR(3),
-nombre VARCHAR(20),
+id_tipousuario VARCHAR(3) NOT NULL,
+nombre VARCHAR(20) NOT NULL,
 descripcion VARCHAR(150)
 
 );
 
 create table recursos(
 
-id_recursos INT,
-nombre VARCHAR(20),
-ubicacion VARCHAR(20),
-capacidad INT,
-tiporecurso INT,
-fecha DATE,
-horario_inicial TIME,
-horario_final TIME
+id_recursos INT NOT NULL,
+nombre VARCHAR(20) NOT NULL,
+ubicacion VARCHAR(20) NOT NULL,
+capacidad INT NOT NULL,
+tiporecurso INT NOT NULL,
+fecha DATE NOT NULL,
+horario_inicial TIME NOT NULL,
+horario_final TIME NOT NULL
 
 );
 
 create table tipo_recurso(
 
-id_tiporecurso INT,
-nombre VARCHAR(20),
-descripcion VARCHAR(22)
+id_tiporecurso INT NOT NULL,
+nombre VARCHAR(20) NOT NULL,
+descripcion VARCHAR(22) NOT NULL
 
 );
+
+--PK
+ALTER TABLE usuarios ADD CONSTRAINT pk_usuario PRIMARY KEY (documento);
+ALTER TABLE tipo_usuario ADD CONSTRAINT pk_tipousuario PRIMARY KEY (id_tipousuario);
+ALTER TABLE recursos ADD CONSTRAINT pk_recursos PRIMARY KEY (id_recursos);
+ALTER TABLE tipo_recurso ADD CONSTRAINT pk_tipo_recurso PRIMARY KEY (id_tiporecurso);
+
+--FK
+ALTER TABLE usuarios ADD FOREIGN KEY (tipousuario) REFERENCES tipo_usuario(id_tipousuario);
+ALTER TABLE recursos ADD FOREIGN KEY (tiporecurso) REFERENCES tipo_recurso(id_tiporecurso);
+
+--UK
+ALTER TABLE usuarios ADD CONSTRAINT uk_correo UNIQUE (correo);
+
+--drop 
+drop table usuarios;
+drop table recursos;
+drop table tipo_usuario;
+drop table tipo_recurso;
+
+
+
+
