bf913a862724d5b7fa5a12ce3dab509248b9840f
unknown <jose.olarte@labinfo.is.escuelaing.edu.co>
2022-04-09 08:51:23 -0500

    Scripts

---
+++ Scripts/Creación Tabla.sql
@@ -0,0 +1,38 @@
+create table usuarios(
+
+documento INT,
+nombre VARCHAR(20),
+contraseña VARCHAR(16),
+correo VARCHAR(20),
+telefono VARCHAR(20),
+tipousuario VARCHAR(3),
+
+);
+
+create table tipo_usuario(
+
+id_tipousuario VARCHAR(3),
+nombre VARCHAR(20),
+descripcion VARCHAR(150)
+
+);
+
+create table recursos(
+
+id_recursos INT,
+nombre VARCHAR(20),
+ubicacion VARCHAR(20),
+capacidad INT,
+tiporecurso INT,
+disponibilidad BOOLEAN,
+horario DATE,
+
+);
+
+create table tipo_recurso(
+
+id_tiporecurso INT,
+nombre VARCHAR(20),
+descripcion VARCHAR(22),
+
+);
