01e71d3fac286dff20978b441635148b47b43d62
Camilo Archila <esteban.archila@mail.escuelaing.edu.co>
2022-04-12 23:17:23 -0500

    Primer avance html

--- src/main/java/edu/eci/cvds/persistence/UserDAO.java
+++ src/main/java/edu/eci/cvds/persistence/UserDAO.java
@@ -1,9 +1,4 @@
 package edu.eci.cvds.persistence;
 
-import edu.eci.cvds.entities.User;
-import org.apache.ibatis.exceptions.PersistenceException;
-
-import java.util.List;
-
 public interface UserDAO {
 }
