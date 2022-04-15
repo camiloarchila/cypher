398962a5b7f318c3b21b9a9a1a1c267aa13512aa
laura valentina <jennyyana@hotmail.com>
2022-04-15 16:05:38 -0500

    tablas v

--- Tables.sql
+++ Tables.sql
@@ -6,7 +6,7 @@ nombre VARCHAR(20) NOT NULL,
 contrasena VARCHAR(16) NOT NULL,
 correo VARCHAR(20) NOT NULL,
 telefono VARCHAR(20),
-tipousuario VARCHAR(3) NOT NULL
+tipousuario INT NOT NULL
 
 );
 
@@ -23,18 +23,30 @@ horario_final INT NOT NULL
 
 );
 
+create table TipoUsuario(
+id INT not null,
+nombre VARCHAR(10) not null
+);
 
 --PK
 ALTER TABLE usuarios ADD CONSTRAINT pk_usuario PRIMARY KEY (documento);
 ALTER TABLE recursos ADD CONSTRAINT pk_recursos PRIMARY KEY (id_recursos);
+ALTER TABLE TipoUsuario  ADD CONSTRAINT pk_tipousuario PRIMARY KEY (id);
 
 
 --UK
 ALTER TABLE usuarios ADD CONSTRAINT uk_correo UNIQUE (correo);
 
+--FK
+alter TABLE usuarios  
+add foreign key (tipousuario) references tipousuario (id);
+
 --Poblar
 insert into recursos (id_recursos,nombre,ubicacion,capacidad,tiporecurso,fecha,horario_inicial,horario_final)
 values (1,'libro','biblioteca1',4,'sala','2020-11-11',5,6);
+
+insert into recursos (id_recursos,nombre,ubicacion,capacidad,tiporecurso,fecha,horario_inicial,horario_final)
+values (2,'libro2','campus',2,'sala','2020-11-11',7,8);
 
 --drop 
 drop table usuarios;
